3a74c66a026d0cc2152d03a34017b6984cf78c25
zeeshanrais <developerxone@hotmail.com>
2023-12-17 21:57:15 +0300

    first checked-in with "hello world prog"

---
+++ core-java/helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+
+        for (int i=0; i<10 ; i++){
+            System.out.print(i+ " ");
+        }
+    }
+}
