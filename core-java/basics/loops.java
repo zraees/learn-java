a058e538775eb2893430505843b2c83d063949c3
zeeshanrais <developerxone@hotmail.com>
2023-12-22 18:15:42 +0300

    first checked-in with "hello world prog"

---
+++ core-java/basics/loops.java
@@ -0,0 +1,37 @@
+package basics;
+
+public class loops {
+    public static void main(String[] args) {
+
+        // loop = repeats statement multiple times
+
+        // 1 - For loop
+        // we know start, termination cond, and jumps (inc/dec)
+        System.out.println("FOR-LOOP");
+        for (int i=0; i<=10; i=i+2){
+            System.out.println("val = " + i);
+        }
+
+        // 2 - while loop -- condition-based loop (entry control loop)
+        System.out.println("WHILE-LOOP");
+        int idx=10;
+        while (idx > 0){
+            System.out.println("val = " + idx);
+            idx-=2;
+        }
+
+        // 3 - do while loop (exit control loop)
+        System.out.println("DO-WHILE-LOOP");
+        do {
+            System.out.println("val = " + idx);
+            idx+=3;
+        } while(idx<=10);
+
+        // 4 - foreach Loop -- enhanced for loop
+        System.out.println("FOREACH-LOOP");
+        String[] names = {"Zeeshan","Rais"};
+        for(String name: names) {
+            System.out.println("name " + name);
+        }
+    }
+}
